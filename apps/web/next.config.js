495537df6cafac8684fcb2dc39144adfdbc44539
Sami-07 <s.a.sami359359@gmail.com>
2025-02-28 23:40:32 +0530

    test: ci cd for frontend

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Disable static page generation for pages that need dynamic data
-  output: 'standalone',
-  // Enable static optimization where possible
-  experimental: {
-    // Enable streaming features
-    serverActions: true,
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
